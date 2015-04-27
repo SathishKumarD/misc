7b76e7f77325e4b6d91d8520502bc997aadaea26
Sathish Kumar Deivasigamani <sdeivasi@buffalo.edu>
2015-04-26 22:49:50 -0400

    write to cvs file
    write to cvs file with pipe character as delimiter

--- subboard_webcrawler.py
+++ subboard_webcrawler.py
@@ -3,7 +3,7 @@ from lxml import html
 import requests
 from lxml import etree
 
-for lis in range(7493, 6700, -1):
+for lis in range(7520, 6700, -1):
     url = 'http://subboard.com/och/listing.asp?id=%s'%lis
     page = requests.get(url)
     #print page.text.tostring()
@@ -13,10 +13,49 @@ for lis in range(7493, 6700, -1):
     if "/och/listing.asp" in pagetext :
         continue
         
-    
-    #e = root.xpath('.//a[text()="TEXT A"]')
+    #print url
     lease = tree.xpath('//dt[text()="Lease"]/following-sibling::*/text()')[0]
+    Bedrooms = tree.xpath('//dt[text()="Bedrooms"]/following-sibling::*/text()')[0]
+    availability = Bedrooms.split()
+    
+    #print availability
+    with open('C:\\Users\\Sathish\\Dropbox\\apt_search\\listing_all.csv', 'a') as the_file:
+        if ("6" in lease or "5" in lease or "7" or "12" in lease):
+            rent = tree.xpath('//dt[text()="Rent"]/following-sibling::dd/div/text()')
+            location = tree.xpath('//*[@id="content"]/dl/dd[1]/text()')
+            row = ''
+            #print url
+            row+=url+'|'
+            #print 'location', location[0].replace('\r\n\t\t\t\t\t','')+','+location[1]
+            row+=location[0].replace('\r\n\t\t\t\t\t','')+', '+location[1]+'|'
+            #print 'lease', lease
+            row+=lease+'|'
+            #print 'rent:' ,rent[0]
+            row+=rent[0]+'|'
+            #print 'contact:',rent[1]
+            row+=rent[1]+'|'
+            
+            #print 'Phone:' ,rent[2]
+            row+=rent[2]+'|'
+            if len(rent) >3:
+                #print 'Best time' , rent[3] 
+                row+=rent[3]+'|'
+                
+            if len(rent) >4:
+                #print 'email' , rent[4] 
+                row+=rent[4]+'|'
+            #print 'Bedrooms', Bedrooms
+            row+=Bedrooms+'\n'
+            the_file.write(row)
+            print '***************************'
+        else:
+            availability
+        
+
+    
+    
     #print lease
+    """
     if "6" in lease or "5" in lease or "7" in lease:
         rent = tree.xpath('//dt[text()="Rent"]/following-sibling::dd/div/text()')
         Bedrooms = tree.xpath('//dt[text()="Bedrooms"]/following-sibling::*/text()')[0]
@@ -30,7 +69,11 @@ for lis in range(7493, 6700, -1):
         if len(rent) >4:
             print 'email' , rent[4] 
         print 'Bedrooms', Bedrooms
-        print '******************************'
+        print '******************************' 
+        """
+    
+    
         
     
+
 
