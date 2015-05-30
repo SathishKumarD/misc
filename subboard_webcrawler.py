fddfd3d953f2b5d51c6b210e85c201f5904e429f
Sathish Kumar Deivasigamani <sdeivasi@buffalo.edu>
2015-05-30 03:47:02 -0400

    Added distance to nearest bus stop
    Google distance matrix api is used

--- subboard_webcrawler.py
+++ subboard_webcrawler.py
@@ -2,9 +2,64 @@ import pymysql
 from lxml import html
 import requests
 from lxml import etree
+import re
+import json
 
-for lis in range(7520, 6700, -1):
-    url = 'http://subboard.com/och/listing.asp?id=%s'%lis
+
+#global constants
+apikey=''  #your API key https://developers.google.com/maps/documentation/distancematrix/
+busstops =['Main circle, Buffalo,NY' , 'Goodyear Hall, Buffalo,NY','Grover Cleveland Highway and Maynard Drive bus stop','Student Union Building, Putnam Way, Buffalo','Flint Loop, Buffalo, NY','Ellicott Complex, Buffalo, NY','Flint Road and UB Service Road, Amherst, NY']
+mapsapiurl = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=[[origin]]&destinations=[[destination]]&mode=walking&key=[[apikey]]'
+suboardurl ='http://subboard.com/och/listing.asp?id=%s'
+listingStart,listingEnd = 7589, 7000
+
+
+def sanitizeAddress(address):
+    address = address.lower()
+    stopwords =[',','.','apt','lower','upper','#']
+    for s in stopwords:
+        address =address.replace(s,'')
+    address = re.sub(' +',' ',address)
+    address = address.strip().replace(' ','+')
+    return address
+    
+#get the walking distance, duration to the nearest busstop
+def getWalkingDistanceToBusstop(originaddr):
+    originaddr = sanitizeAddress(originaddr)
+    
+    print mapsapiurl.replace('[[origin]]',originaddr).replace('[[destination]]',sanitizeAddress(busstops[0]))
+    mindis= 100000
+    mindisstop ='Not Found'
+    mindur =0
+    for busstop in busstops:
+        stop = sanitizeAddress(busstop)
+        url = mapsapiurl.replace('[[origin]]',originaddr).replace('[[destination]]',stop).replace('[[apikey]]',apikey)
+        #print url
+        page = requests.get(url)
+        #print page.text
+        distancejson = json.loads(page.text)
+        
+         
+        if distancejson['rows'][0]['elements'][0]['status'] == 'NOT_FOUND':
+            print 'Cannot find distance'
+            return str(mindis),mindisstop,mindur
+        
+        dis = distancejson['rows'][0]['elements'][0]['distance']['value']
+        duration = int(distancejson['rows'][0]['elements'][0]['duration']['value'])/60
+        dist = float(dis)*0.000621371
+        #print busstop , dist,mindis, duration
+        if dist < mindis:
+            mindis = dist
+            mindisstop = busstop
+            mindur = duration
+            
+    
+    mindis = '%.2f' %mindis
+    #print mindis,mindisstop,mindur
+    return mindis,mindisstop,mindur
+
+for lis in range(listingStart, listingEnd, -1):
+    url = suboardurl%lis
     page = requests.get(url)
     #print page.text.tostring()
     #print page
@@ -19,7 +74,7 @@ for lis in range(7520, 6700, -1):
     availability = Bedrooms.split()
     
     #print availability
-    with open('C:\\Users\\Sathish\\Dropbox\\apt_search\\listing_all.csv', 'a') as the_file:
+    with open('/Users/sathish/misc/listing_all.csv', 'a') as the_file:
         if ("6" in lease or "5" in lease or "7" or "12" in lease):
             rent = tree.xpath('//dt[text()="Rent"]/following-sibling::dd/div/text()')
             location = tree.xpath('//*[@id="content"]/dl/dd[1]/text()')
@@ -28,6 +83,12 @@ for lis in range(7520, 6700, -1):
             row+=url+'|'
             #print 'location', location[0].replace('\r\n\t\t\t\t\t','')+','+location[1]
             row+=location[0].replace('\r\n\t\t\t\t\t','')+', '+location[1]+'|'
+            mindis,mindistop,duration = getWalkingDistanceToBusstop(location[0].replace('\r\n\t\t\t\t\t','')+', '+location[1])
+            #print mindis,mindistop,duration
+            
+            row+=mindis +'|'
+            row+=mindistop +'|'
+            row+=str(duration) +'|'
             #print 'lease', lease
             row+=lease+'|'
             #print 'rent:' ,rent[0]
@@ -46,6 +107,13 @@ for lis in range(7520, 6700, -1):
                 row+=rent[4]+'|'
             #print 'Bedrooms', Bedrooms
             row+=Bedrooms+'\n'
+            
+            ## windows machine hack revert it back
+            row = row.replace(',','```')
+            row = row.replace('|',',')
+            
+            ## hack ends
+            
             the_file.write(row)
             print '***************************'
         else:
@@ -54,7 +122,7 @@ for lis in range(7520, 6700, -1):
 
     
     
-    #print lease
+    #print lease - only 6 or 5 or 7 months lease
     """
     if "6" in lease or "5" in lease or "7" in lease:
         rent = tree.xpath('//dt[text()="Rent"]/following-sibling::dd/div/text()')
